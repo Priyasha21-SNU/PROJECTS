1bf5005fe28bcaf6107f2d65ab80f0f1c30ddd45
PRIYASHA SINGHA <95346970+Priyasha21-SNU@users.noreply.github.com>
2023-10-09 13:03:56 +0530

    Add files via upload
    its a react app of car booking

---
+++ BookingScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function BookingScreen() {
+  return (
+    <View>
+      <Text>Booking Screen</Text>
+      {/* Add UI components and logic for booking a car */}
+    </View>
+  );
+}
---
+++ CarDetailsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function CarDetailsScreen() {
+  return (
+    <View>
+      <Text>Car Details Screen</Text>
+      {/* Add UI components and logic for displaying car details */}
+    </View>
+  );
+}
